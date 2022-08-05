3fb8f1da38ecc31be7c9358846866d115d265e85
m-godlewski <mgodlewski25@gmail.com>
2022-08-06 00:27:48 +0200

    Modified air observer script.

--- scripts/air.py
+++ scripts/air.py
@@ -1,3 +1,7 @@
+"""
+House air status observer script.
+"""
+
 import config
 import os
 from datetime import datetime
@@ -6,32 +10,38 @@ import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 
+# influx database bucket name
 BUCKET = "air"
 
-influx = influxdb_client.InfluxDBClient(
+
+# initializing database connection
+database_client = influxdb_client.InfluxDBClient(
     url = config.DB["INFLUX"]["URL"],
     token = config.DB["INFLUX"]["API_TOKEN"],
     org = config.DB["INFLUX"]["ORGANIZATION"]
 )
-influxdb_api = influx.write_api(
-    write_options=SYNCHRONOUS
-)
 
+# database api
+database_api = database_client.write_api(write_options=SYNCHRONOUS)
+
+# retrieving air status from device
 air_status = os.popen(
     f"miiocli airpurifiermiot --ip 192.168.0.101 --token {config.DEVICES['TOKENS']['Mi Air Purifier 3H']} status"
 ).read()
 air_status = air_status.split("\n")
 
+# variables that stores air status
 aqi = int(air_status[2].split(":")[1].strip().split(" ")[0])
-
 humidity = int(air_status[4].split(":")[1].strip().split(" ")[0])
-
 temperature = float(air_status[5].split(":")[1].strip().split(" ")[0])
 
+# preparing data for inserting to influx database
 point = influxdb_client.Point("air").tag("room", "bedroom").field("aqi", aqi).field("humidity", humidity).field("temperature", temperature)
-influxdb_api.write(
-    bucket=BUCKET,
-    org=config.DB["INFLUX"]["ORGANIZATION"],
-    record=point
+
+# inserting data to influx database
+database_api.write(
+    bucket = BUCKET,
+    org = config.DB["INFLUX"]["ORGANIZATION"],
+    record = point
 )
 print(f"{datetime.now()} \t\t METRIC: bedroom \t\t VALUES: {aqi}, {humidity}, {temperature}")
