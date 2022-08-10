37922eebec89ca5b9308ce8af989e8c21dd060d3
Mateusz <mgodlewski25@gmail.com>
2022-08-11 00:29:46 +0200

    Added "rooms" django app

--- central/devices/models.py
+++ central/devices/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from rooms.models import Room
+
 
 class Device(models.Model):
     """"""
@@ -7,6 +9,7 @@ class Device(models.Model):
     name = models.CharField(max_length=50)
     brand = models.CharField(max_length=50)
     mac_address = models.CharField(max_length=17)
+    location = models.ForeignKey(Room, null=True, on_delete=models.SET_NULL)
 
     def __str__(self) -> str:
         """Returns representation of object in form of string."""
