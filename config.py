035117d91f10d6b7589678263be66afc9631f762
m-godlewski <mgodlewski25@gmail.com>
2023-04-21 00:44:06 +0200

    CHANGELOG, README and config updated.

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 
 # current system version
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 
 # absolute path to scripts directory
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
