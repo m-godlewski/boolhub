53f9ce28cd1dac15e6bba34d5baf064e8cc85c48
m-godlewski <mgodlewski25@gmail.com>
2024-02-02 10:14:18 +0100

    title and priority added to notifications

--- scripts/messenger.py
+++ scripts/messenger.py
@@ -9,7 +9,7 @@ import traceback
 import requests
 
 
-def send_notification(text: str) -> int:
+def send_notification(text: str, title: str, priority: int = 3) -> int:
     """Sends notification to 'ntfy' app server with predefined subject
     and string received by argument as notification content. Returns HTTP status code.
     """
@@ -17,6 +17,7 @@ def send_notification(text: str) -> int:
         response = requests.post(
             url=config.SCRIPTS["MESSENGER"]["NTFY_SERVER_URL"],
             data=text.encode("utf-8"),
+            headers={"Title": title.encode("utf-8"), "Priority": str(priority)},
         )
     except Exception:
         logging.error(f"Unknown error occurred!\n{traceback.format_exc()}")
