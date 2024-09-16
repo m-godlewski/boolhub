26413f53f5d2c5d1429d68549240dcff5f014066
Mateusz Godlewski <mgodlewski25@gmail.com>
2024-09-16 19:13:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ echo '###                                            ###'
 echo '##################################################'
 
 echo "\n\n\nStep 0. Update System"
-apt-get update
-apt-get upgrade
+apt-get update -y
+apt-get upgrade -y
 
 echo "\n\n\nStep 1. OpenSSH Installation"
 apt-get install openssh-client
@@ -17,8 +17,8 @@ echo "\n\n\nStep 2. Net Tools Installation"
 apt-get install net-tools
 
 echo "\n\n\nStep 3. Installing Python PIP and VENV"
-apt-get install python3-pip
-apt-get install python3.8-venv
+apt-get install python3-pip -y
+apt-get install python3.8-venv -y
 
 echo "\n\n\nStep 4. Configuring Time Zone"
 # echo "To display list of available timezones use command '$ timedatectl list-timezones'."
@@ -27,7 +27,7 @@ echo "\n\n\nStep 4. Configuring Time Zone"
 
 echo "\n\n\nStep 5. Installing Docker"
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove $pkg; done
-apt-get update
+apt-get update -y
 apt-get install ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -36,10 +36,9 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-apt-get install docker-compose
+apt-get update -y
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+apt-get install docker-compose -y
 groupadd docker
 usermod -aG docker $USER
 newgrp docker
-apt-get install docker-compose
