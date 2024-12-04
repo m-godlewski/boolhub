8b766267e76bc09b715ba9dcf2a6f84adf17de74
m-godlewski <mgodlewski25@gmail.com>
2024-12-04 13:34:10 +0100

    Remove constance remainings

--- central/central/settings.py
+++ central/central/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "constance",
     "corsheaders",
     "rest_framework",
     "settings.apps.SettingsConfig",
